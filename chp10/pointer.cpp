779d5b89b2deceefe2c5e2b2c598af8012402dfb
SeBeom12 <sebeom6135@naver.com>
2024-05-17 12:09:24 +0900

    chp10

---
+++ chp10/pointer.cpp
@@ -0,0 +1,27 @@
+//pointer.cpp
+#include <iostream>
+
+using namespace std;
+/*int main() {
+	int x = 5;
+
+
+
+	//p´Â x¸¦ °¡¸®Å´
+
+	int *p = &x;
+	cout << "x =  " << x << endl;
+	cout << "&x = " << &x << endl;
+	cout << "p = " << *p << endl;
+
+	*p = 10;
+
+	cout << "x =  " << x << endl;
+	cout << "&x = " << &x << endl;
+	cout << "p = " << p << endl;
+
+	return 0;
+
+	
+
+}
